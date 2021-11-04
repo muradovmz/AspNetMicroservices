58509cf0b5caa345183100b8c7232f590b07d0b2
Mikheil Muradov <Mikheil.Muradov@space.ge>
2021-11-04 17:13:25 +0400

    part of ordering service

---
+++ src/Services/Ordering/Ordering.Infrastucture/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastucture.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                                .Where(o => o.UserName == userName)
+                                .ToListAsync();
+            return orderList;
+        }
+    }
+}
